ddd560b98fda3a2fb99f001153e0ab991c790cf9
tianqinisnan <smile241isnan@163.com>
2016-11-18 11:39:10 +0800

    MOD:datapicker

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,17 @@
             });
         });
     }
+    var options = {};
+    options.ranges = {
+      'Today': [moment(), moment()],
+      'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+      'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+      'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+      'This Month': [moment().startOf('month'), moment().endOf('month')],
+      'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+    };
+
+    $('#demo').daterangepicker(options, function(start, end, label) {
+        console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')'); 
+    });
 })(jQuery);
